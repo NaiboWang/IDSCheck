44e0f1879de2be235a2b03eeb71bfeee41c3d00c
Naibo Wang <naibowang@u.nus.edu>
2022-11-05 15:33:06 +0800

    Version 2.2

---
+++ frp.sh
@@ -0,0 +1,5 @@
+wget https://github.com/fatedier/frp/releases/download/v0.44.0/frp_0.44.0_linux_amd64.tar.gz
+tar xvf frp_0.44.0_linux_amd64.tar.gz
+rm frp_0.44.0_linux_amd64.tar.gz
+mv frp_0.44.0_linux_amd64 frp
+cd frp
